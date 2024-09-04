8831631f50428a5e28f30a0e5fd8ca7cce8ea381
cowboysj <su10jin11@khu.ac.kr>
2024-09-04 12:17:06 +0900

    [fix] : database 설정

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:12345678@localhost/mimo"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://admin:12345678@danghag2.ctaeyoe8uvha.ap-northeast-2.rds.amazonaws.com/danghag"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
