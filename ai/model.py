8737874236434573046215dd4d3280f2c4209572
cowboysj <su10jin11@khu.ac.kr>
2024-09-04 13:39:31 +0900

    [Fix] : stable-diffusion

--- ai/model.py
+++ ai/model.py
@@ -1,5 +1,4 @@
 import torch
-from PIL import Image
 from diffusers import StableDiffusionPipeline
 from huggingface_hub import login
 import os
@@ -8,8 +7,8 @@ import os
 HF_ACCESS_TOKEN = os.getenv("HF_ACCESS_TOKEN")
 login(HF_ACCESS_TOKEN)
 
-
-pipeline = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+# Stable Diffusion v1-4 모델로 변경
+pipeline = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4")
 
 def sampling(prompts, batch_size=1):
     """
@@ -19,7 +18,7 @@ def sampling(prompts, batch_size=1):
     :param batch_size: 한 번에 생성할 이미지 수
     :return: 생성된 이미지 목록 (PIL.Image 객체)
     """
-    model_id = "runwayml/stable-diffusion-v1-5"  # 사용할 Stable Diffusion 모델 ID
+    model_id = "CompVis/stable-diffusion-v1-4"  # 사용할 Stable Diffusion 모델 ID
     
     # Stable Diffusion 모델을 로드합니다.
     pipe = StableDiffusionPipeline.from_pretrained(
@@ -31,13 +30,12 @@ def sampling(prompts, batch_size=1):
     # Safety Checker 비활성화
     pipe.safety_checker = None
 
-
     # GPU가 사용 가능하면 GPU로 모델을 이동합니다.
     if torch.cuda.is_available():
         pipe = pipe.to("cuda")
         print("cuda 사용")
     
     # 이미지를 생성합니다.
-    images = pipe(prompt=prompts, num_inference_steps=1).images
+    images = pipe(prompt=prompts, num_inference_steps=50).images  # num_inference_steps를 더 늘릴 수 있습니다.
 
     return images
